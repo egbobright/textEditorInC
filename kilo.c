7655226605fca59547c6496670ef2ca780edb4f5
boe <egbobright@gmail.com>
2025-11-12 22:01:33 +0100

    added more utility

--- kilo.c
+++ kilo.c
@@ -5,55 +5,74 @@
 #include <errno.h>
 #include <ctype.h>
 
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
 
+
+
+/*** data **/
 struct termios orig_termios;
-
+/*** terminal ***/
 void die (const char *s){
    perror(s);
    exit(1);
 }
 
-void disableRawMode(void){
-  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
-     die("tcsetattr");
+void disableRawMode(){
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
 }
 
-void enableRawMode(void){
-  if(tcgetattr(STDIN_FILENO, &orig_termios)== -1)die("tcgetattr");
+void enableRawMode(){
+  if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
    atexit(disableRawMode);
      
 
    struct termios raw = orig_termios;
    
    //tcgetattr(STDIN_FILENO, &raw);
-   raw.c_lflag &= ~(BRKINT |ICRNL |INPCK|ISTRIP |IXON);
+   raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
    raw.c_lflag &= ~(OPOST);
    raw.c_lflag |= (CS8);
    raw.c_lflag &= ~(ECHO | ICANON |IEXTEN | ISIG);
    raw.c_cc[VMIN] = 0;
    raw.c_cc[VTIME] = 1; 
-   if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)== -1) die("tcsetattr");
+   if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 
+char editorReadKey(){
+  int nread;
+  char c;
+  while((nread = read(STDIN_FILENO, &c, 1)) != 1){
+    if(nread == -1 && errno != EAGAIN) die("read");
+   }
+ return c;
+}
 
+/*** input ***/
 
+void editorProcessKeyPress(){
+  char c = editorReadKey();
+
+  switch(c){
+     case CTRL_KEY('q'):exit(0);
+     break;
+   }
+}
+
+/*** output ***/
+void editorRefreshScreen(){
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+}
 
 
 
 int main(void){
   enableRawMode(); 
-
    while(1){
-   char c = '\0';
-   //while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
-    if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-       if(iscntrl(c)){
-            printf("%d\n", c); 
-          }else{
-           printf("%d ('%c')\n", c, c);
-           }
-       if(c == 'q') break;
-    }
+    editorRefreshScreen();
+    editorProcessKeyPress();     
+  }
    return 0;
+  
  }
